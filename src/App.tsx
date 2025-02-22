c2f5f8028981a6f4aedb1abe2f8041c567a7db82
Peter Kwamena Essibu <164653045+peterkessibu@users.noreply.github.com>
2025-02-22 00:17:02 +0000

    Add theme toggle button to switch between light and dark modes in App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,49 @@
 import { useState } from "react";
-import { Container, Box, Typography, CssBaseline } from "@mui/material";
+import { Container, Box, Typography, CssBaseline, Button } from "@mui/material";
 import { CurrentWeather } from "./components/CurrentWeather";
 import { Forecast } from "./components/ForeastWeather";
 import { LocationSearch } from "./components/Autocompletes";
 import { ThemeProvider, createTheme } from "@mui/material/styles";
+import { Brightness4, Brightness7 } from "@mui/icons-material";
 
 const App = () => {
   const [location, setLocation] = useState<string | null>(null);
+  const [mode, setMode] = useState<"light" | "dark">("light");
 
-  const darkTheme = createTheme({
+  const theme = createTheme({
     palette: {
-      mode: "light",
+      mode,
     },
   });
+
   return (
-    <ThemeProvider theme={darkTheme}>
+    <ThemeProvider theme={theme}>
       <CssBaseline />
       <Container maxWidth="lg">
         <Box sx={{ my: 4, textAlign: "center" }}>
-          <Typography
-            data-testid="Weather-app"
-            variant="h3"
-            component="h1"
-            gutterBottom
-          >
-            Weather App
-          </Typography>
+          <Container>
+            <Typography
+              data-testid="Weather-app"
+              variant="h3"
+              component="h1"
+              gutterBottom
+            >
+              Weather App
+            </Typography>
+            <Button
+              variant="contained"
+              startIcon={mode === "light" ? <Brightness4 /> : <Brightness7 />}
+              onClick={() => setMode(mode === "light" ? "dark" : "light")}
+              sx={{mb: 3 }}
+            >
+              {mode === "light" ? "Dark Mode" : "Light Mode"}
+            </Button>
+          </Container>
+
           <LocationSearch
             onLocationSelect={(location) => setLocation(location)}
           />
+
           {location && (
             <>
               <CurrentWeather location={location} />
